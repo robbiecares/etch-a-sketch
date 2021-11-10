986dc9a74d5f1e672580655c354e23d6404dbc56
robbiecares <robbiecares@gmail.com>
2021-11-10 16:17:36 +0100

    updated "drag fill" behavoir to be included the first pixel, implemented greyscale fill mode

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
+// set global variables
 const easBody = document.getElementById('eas-body')
 const resetBtn = document.getElementById('reset-btn')
-const radioBtn = document.getElementsByName('fill-type')
+const radioBtn = document.getElementsByName('fill-mode')
 let pixelArea = document.createElement('div')
 let mouseDown = false
 const defaultGridSize = 60;
-const maxGridSize = 100;
+const maxGridSize = 10;
 let gridSize = maxGridSize
 
 // nodelist of all pixels
 let grid
 
-// set the initial fill mode for a pixel
-let fillMode = 'standard-fill'
+// set initial fill mode for a pixel
+let fillMode = document.querySelector('input[name="fill-mode"]:checked')
+fillMode = fillMode.value
+ 
+const psychedelicColors = ['#ffb400', '#ff9500', '#5fbeff', '#ff1daa', '#6bd13e']
+let psychedelicColor = getPsychedelicColor()
 
 // setup listeners
 resetBtn.addEventListener('click', resetPixelArea)
 
-// sets the status of the mouse button as up or down (used to fill pixels)
+// updates variables to assist with the pixel 'dragFill()' behavoir
 pixelArea.addEventListener('mousedown', () => {
     mouseDown = true
     // console.log(mouseDown)
     });
 pixelArea.addEventListener('mouseup', () => {
     mouseDown = false
+    psychedelicColor = getPsychedelicColor()
     // console.log(mouseDown)
     });
+
+// sets a boundary to prevent the mouse from being held down outside of the pixel-area
+pixelArea.addEventListener('mouseleave', () => {mouseDown = false})
 
 // updates the desired fill mode
 radioBtn.forEach(option => 
@@ -46,8 +55,11 @@ function setupPage() {
     // creates a grid of pixels & attachs it to the pixel area
     grid = createGrid(gridSize)   
 
-    // set grid filling functionality
-    grid.forEach(pixel => pixel.addEventListener('mouseover', fillSelected))
+    // set listeners for pixel fill behavoir
+    grid.forEach(pixel => pixel.addEventListener('mousedown', downFill))
+    grid.forEach(pixel => pixel.addEventListener('mouseenter', entryFill))
+    // grid.forEach(pixel => pixel.addEventListener('click', clickFill))
+    
 }
 
 function determineGridSize() {
@@ -82,18 +94,111 @@ function createGrid (size) {
     return document.querySelectorAll('.pixel')
 }
 
-function fillSelected() { 
-    if (mouseDown) {
-        this.classList.add(fillMode)}
+function downFill() {
+    // sets pixel fill behavoir when the mouse button is pushed down
+    switch (fillMode) {
+        case 'standard-fill':
+            this.style.backgroundColor = 'grey'
+            break;
+        case 'psychedelic-fill':
+            this.style.backgroundColor = psychedelicColor
+            break;
+        case 'greyscale-fill':
+            this.style.backgroundColor = greyscaleFill(this)
+            break;
+    
+    // if (mouseDown && fillMode == 'greyscale-fill') {
+    //     this.style.backgroundColor = greyscaleFill(this)}
+    }   
 }
 
+function entryFill() { 
+    // sets pixel fill behavoir while holding down the mouse button and dragging the cursor into a new pixel
+    if (mouseDown) {
+        switch (fillMode) {
+            case 'standard-fill':
+                this.style.backgroundColor = 'grey'
+                break;
+            case 'psychedelic-fill':
+                this.style.backgroundColor = psychedelicColor
+                break;
+            case 'greyscale-fill':
+                this.style.backgroundColor = greyscaleFill(this)
+        }
+    }
+}
+
+function clickFill() {
+    // sets pixel fill behavoir on mouse click
+    switch (fillMode) {
+        case 'standard-fill':
+            this.style.backgroundColor = 'grey'
+            break;
+        case 'psychedelic-fill':
+            this.style.backgroundColor = psychedelicFill()
+            break;
+        case 'greyscale-fill':
+            this.style.backgroundColor = greyscaleFill(this)
+            break;
+
+    }
+}
+
+function getPsychedelicColor() {
+    // return a random color from the psychedelic pallet
+    return psychedelicColors[Math.floor(Math.random() * psychedelicColors.length)]
+
+}
+
+function greyscaleFill(pixel) {
+    // sets a pixel's color on an ascending scale from white to black
+
+    const currentBgColor = pixel.style.backgroundColor
+    
+    // extracts array of currentBgColor's rgb values
+    let rgbComponenets = currentBgColor.substring(4, currentBgColor.length-1).replace(/ /g, '').split(',');
+    for (i = 0; i < rgbComponenets.length; i++) {
+        rgbComponenets[i] = Number(rgbComponenets[i])
+    }
+
+    // converts currentBgColor from rgb to hex
+    const currentBgColorHex = ConvertRGBtoHex(rgbComponenets[0], rgbComponenets[1], rgbComponenets[2])
+    
+    // if pixel is already black, do not change the color
+    if (currentBgColor == 'black')
+        bgColor = 'black'
+
+    // sets the pixel color to white if pixel is unfilled or has been filled by another fill method
+    else if (!currentBgColor || psychedelicColors.includes(currentBgColorHex) || currentBgColor == 'grey') {
+        bgColor = 'rgb(255, 255, 255)'
+        
+    // makes then pixel 10% darker than it's current shade (starting from white, ending with black)
+    } else {
+        // subtract 26 (i.e. 10%) from each RGB component in currentBgColor
+        for (i = 0; i < rgbComponenets.length; i++) {
+            rgbComponenets[i] -= 26
+            }
+        bgColor = `rgb(${rgbComponenets[0]}, ${rgbComponenets[1]}, ${rgbComponenets[2]})`
+    }
+
+    return bgColor
+}
+
+function ConvertRGBtoHex(red, green, blue) {
+    return "#" + ColorToHex(red) + ColorToHex(green) + ColorToHex(blue);
+}
+
+function ColorToHex(color) {
+    var hexadecimal = color.toString(16);
+    return hexadecimal.length == 1 ? "0" + hexadecimal : hexadecimal;
+}
+  
 function resetPixelArea() {
 
     pixelArea.textContent = ''
     
     setupPage()   
 }
-
 
 setupPage()
 
